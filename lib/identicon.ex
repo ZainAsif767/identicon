883567dc071a94ce96b254074911c4f264c29c0b
zainasif767 <zainclasher2002@gmail.com>
2023-01-31 00:28:14 +0500

    Feat: Updating def build_grid

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -9,9 +9,14 @@ defmodule Identicon do
   def build_grid(image) do
     %Identicon.Image{hex: hex} = image
 
-    hex
-    |> Enum.chunk_every(3, 3, :discard)
-    |> Enum.map(&mirror_row/1)
+    grid =
+      hex
+      |> Enum.chunk_every(3, 3, :discard)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten()
+      |> Enum.with_index()
+
+    %Identicon.Image{image | grid: grid}
   end
 
   def mirror_row(row) do
