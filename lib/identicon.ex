72a02f5b25ac30898bf1154d5d15ed98d185d7fb
zainasif767 <zainclasher2002@gmail.com>
2023-01-30 23:39:24 +0500

    Fixes

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,12 +3,27 @@ defmodule Identicon do
     input
     |> hash_input()
     |> pick_color()
+    |> build_grid()
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+
+    hex
+    |> Enum.chunk_every(3)
+    |> Enum.map(&mirror_row/1)
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def pick_color(image) do
     # this line here is acknowledging that there are more
     # elements in the list but we don't actually care about them !
     %Identicon.Image{hex: [r, g, b | _tail]} = image
+    %Identicon.Image{image | color: {r, g, b}}
 
     # return rgb
     [r, g, b]
