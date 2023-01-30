6189e4057e8d3450a9792e4dac34e5b20b5262de
zainasif767 <zainclasher2002@gmail.com>
2023-01-30 21:27:38 +0500

    included : [xref: [exclude: [:crypto]]]

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Identicon.MixProject do
       version: "0.1.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      xref: [exclude: [:crypto]]
     ]
   end
 
