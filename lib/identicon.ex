52cc37cafe900611518685b53475680087dc53ae
zainasif767 <zainclasher2002@gmail.com>
2023-01-30 21:48:19 +0500

    Adding Functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,18 +1,27 @@
 defmodule Identicon do
-  @moduledoc """
-  Documentation for `Identicon`.
-  """
+  def main(input) do
+    input
+    |> hash_input()
+    |> pick_color()
+  end
 
-  @doc """
-  Hello world.
+  def pick_color(image) do
+    # this line here is acknowledging that there are more
+    # elements in the list but we don't actually care about them !
+    %Identicon.Image{hex: [r, g, b | _tail]} = image
 
-  ## Examples
+    # return rgb
+    [r, g, b]
+  end
 
-      iex> Identicon.hello()
-      :world
+  def hash_input(input) do
+    # generates a random list of 15 elements
+    hex =
+      :crypto.hash(:md5, input)
+      # converting into list
+      |> :binary.bin_to_list()
 
-  """
-  def hello do
-    :world
+    # using Struct to store our value with hex as property
+    %Identicon.Image{hex: hex}
   end
 end
